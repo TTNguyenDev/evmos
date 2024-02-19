c492b33c6ef748bccb02db4ecdf2c0e964b7060b
TTNguyenDev <nttriet9a4@gmail.com>
2024-02-19 21:44:22 +0700

    tokensell deployment

--- src/TokenSell.sol
+++ src/TokenSell.sol
@@ -6,12 +6,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenSell is ERC20, Ownable {
   uint private _rate;
+  address owner_= 0x4A9C64A927a778a66eA2C71922b0002dBeBB43f5;
 
-event ConversionRateUpdated(uint oldRate, uint newRate);
+  event ConversionRateUpdated(uint oldRate, uint newRate);
+
   constructor(
     string memory name_,
     string memory symbol_
-  ) ERC20(name_, symbol_) {
+  ) ERC20(name_, symbol_)  Ownable(owner_){
     _mint(address(this), 22_000_000 * 1E18);
     _rate = 10000;
   }
@@ -20,10 +22,10 @@ event ConversionRateUpdated(uint oldRate, uint newRate);
     require(msg.value > 0, "Ether to send must be > 0");
     uint tokenAmount = msg.value * _rate;
     require(
-      balanceOf(address(this) > amount, "Not enough tokens")
+      balanceOf(address(this)) > tokenAmount, "Not enough tokens"
     );
 
-    _transfer(address(this), msg.sender, amount);
+    _transfer(address(this), msg.sender, tokenAmount);
   }
 
   function setRate(uint _newRate) external onlyOwner{
